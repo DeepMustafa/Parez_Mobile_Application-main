b7c11ace415855d02441b2f3104f2b644dd7e5d4
Mustafa Khalid Jamal <58405732+DeepMustafa@users.noreply.github.com>
2022-08-31 15:05:15 +0300

    Initial commit

---
+++ lib/Screens/onBoarding/content_model.dart
@@ -0,0 +1,25 @@
+class UnbordingContent {
+  String image;
+  String title;
+  String discription;
+
+  UnbordingContent({this.image, this.title, this.discription});
+}
+
+List<UnbordingContent> contents = [
+  UnbordingContent(
+    title: 'پــــارێـــز',
+    image: "assets/images/women1.svg",
+    discription: "تایبەتە بەو ئافرەتانەی کە بوونەتە قوربانی دەستی کۆمەڵگا و تاوانیان بەرامبەر ئەنجام دەدرێت بە هەر هۆیەک بێت"
+  ),
+  UnbordingContent(
+    title: 'چۆنیەتی چوونەژوورەوە',
+    image: "assets/images/women2.svg",
+    discription: "بۆ کار ئاسانی و بتوانن هەموو ئافرەتان بەکاری بهێنن، تەنها لە ڕێگەی ژمارە مۆبایل دەتوانن داخیل ببن"
+  ),
+  UnbordingContent(
+    title: 'لەگەڵ ئێمە پارێزراوی',
+    image: "assets/images/women3.svg",
+    discription: "لەکاتی دروست کردنی هەژمار، بەتەواوی پارێزراو دەبی لە هەموو دەست درێژی کردنێک بۆ سەر مافەکانت"
+  ),
+];
---
+++ lib/models/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static const apiURL = "https://parez-backend.herokuapp.com/";
+  static const otpVerifyAPI = "https://parez-backend.herokuapp.com/verify-otp/";
+  static const otpLoginAPI = "https://parez-backend.herokuapp.com/otp/";
+}
